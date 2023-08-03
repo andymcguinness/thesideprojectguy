533fc839fecbbf7a5909ecab1134f7c7b2f32466
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2023-08-02 23:56:24 -0400

    Work for the night

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'tspg-yellow': '#fbdb5b',
+        'tspg-gray': '#d9d9d9',
       },
-    },
+    }
   },
   plugins: [],
 }
