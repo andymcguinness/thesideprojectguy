78e9430ab01486e25380452a764aad819c4641ec
Andy McGuinness <michaelandrewmcguinness@gmail.com>
2024-04-30 13:21:53 -0400

    Making OpenAI integration functional again.

--- pages/api/openaiidea.tsx
+++ pages/api/openaiidea.tsx
@@ -1,45 +1,32 @@
 // Next.js
 import type { NextRequest } from "next/server"
 
-// OpenAI package, but the edge version
-import { Configuration, OpenAIApi, ResponseTypes } from "openai-edge"
+// OpenAI package
+import OpenAI from 'openai';
 
 // Set the API key
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-// Init the main component
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
 
 const handler = async (req: NextRequest) => {
 
   try {
 
     // Try getting a name
-    const item_name = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "Generate a side project idea.",
-      max_tokens: 500,
-      stream: false,
+    const item_name = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Generate a side project idea.' }],
+      model: 'gpt-3.5-turbo',
     });
 
-    // Await the response, so we can do something with it
-    const data = (await item_name.json()) as ResponseTypes["createCompletion"];
-
-    console.log(data);
-
     // Yay! We did it! Return that response!
-    return new Response(JSON.stringify(data?.choices[0]?.text?.trim().replace("\n", "").replace("\"", "").replace("\\\"", "")), {
+    return new Response(JSON.stringify(item_name.choices[0].message.content?.trim().replace("\n", "").replace("\"", "").replace("\\\"", "")), {
       status: 200,
       headers: {
         "content-type": "application/json",
       },
     })
   } catch (error: any) {
-
-    // Something's gone sideways -- let's take a look
-    console.error(error)
 
     // Tell the UI the bad news
     return new Response(JSON.stringify(error), {
